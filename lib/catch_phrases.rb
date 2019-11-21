aee56e616d348fe8307ed4e6b84f118b498b601c
Ismael Hasan <ismael.hasan@live.com>
2019-11-21 03:44:57 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,11 +4,11 @@ def mario
 end
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
 
 def link
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts status
+  link_says = "It's Dangerous To Go Alone! Take This."
+  puts link_says
 end
